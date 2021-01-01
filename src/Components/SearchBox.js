e387207097a76b54bea780e2a966af3081a67adf
aniledev <elinamcgill@gmail.com>
2021-01-01 18:30:43 -0500

    feat: JSX elements for SearchBar components

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -4,6 +4,16 @@ import React, { Component } from "react";
 
 export default class SearchBox extends Component {
   render() {
-    return <div className="SearchBox" />;
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar__heading">
+          <h1>File Uploader</h1>
+        </div>
+        <div className="SearchBar__controls">
+          <SearchBox />
+          <FilterOptions />
+        </div>
+      </div>
+    );
   }
 }
