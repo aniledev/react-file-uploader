13a51dd46e25339c8db35fd5b80c0e792301ab93
aniledev <elinamcgill@gmail.com>
2021-01-01 15:23:23 -0500

    file: create file structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./Components/App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+// break the application into single-minded components
+// then decide which data are state and which are props and where state would live in your components hierarchy
+// finally add the interactivity for the user
