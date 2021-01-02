cc1b28a7f90e20b87a5511c6a4b25c01eda6ae0f
aniledev <elinamcgill@gmail.com>
2021-01-02 11:29:46 -0500

    feat: write JSX elements for ControlBar component

--- src/Components/ControlBar.js
+++ src/Components/ControlBar.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 import "../Styles/ControlBar.css";
+//IMPORT FONTAWESOME ICON LIBRARY AND THEN IMPORT SPECIFIC ICONS
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faRedoAlt,
+  faTrashAlt,
+  faCloudDownloadAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default class ControlBar extends Component {
   render() {
-    return <div className="ControlBar" />;
+    return (
+      <div className="ControlBar">
+        <div className="ControlBar__btn">
+          <FontAwesomeIcon icon={faRedoAlt} />
+        </div>
+        <div className="ControlBar__btn">
+          <FontAwesomeIcon icon={faTrashAlt} />
+        </div>
+        <div className="ControlBar__btn">
+          <FontAwesomeIcon icon={faCloudDownloadAlt} />
+        </div>
+      </div>
+    );
   }
 }
