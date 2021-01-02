35ee8b4aac623d12d5fba4f5d3936171d7e76acf
aniledev <elinamcgill@gmail.com>
2021-01-02 10:38:40 -0500

    feat: write JSX elements for FilterableList component

--- src/Components/FIlterableList.js
+++ src/Components/FIlterableList.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
 import "../Styles/FilterableList.css";
+import ListItem from "./ListItem";
 
 export default class FIlterableList extends Component {
   render() {
-    return <div className="FilterableList" />;
+    // CREAT A VARIABLE TO HOLD DATA FROM PROPS
+    const LIST = this.props.files.map((file, key) => (
+      <ListItem {...file} key={key} />
+    ));
+    // WHAT IS GOING ON WITH THE USE OF THE SPREAD OPERATOR HERE?
+    return <div className="FilterableList">{LIST}</div>;
   }
 }
+
+// ESTABLISH DEFAULT PROPS
+FIlterableList.defaultProps = {
+  files: [],
+};
