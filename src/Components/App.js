5bad09a28ce0ec662f5059cc22eb117ec5c39063
aniledev <elinamcgill@gmail.com>
2021-01-02 22:12:26 -0500

    feat: state lives in the lowest common ancestor of SearchBar and FilterableList

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,6 +4,15 @@ import SearchBar from "./SearchBar";
 import FilterableList from "./FilterableList";
 
 export default class App extends Component {
+  // initialize stat using a constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+      filterOption: "All",
+    };
+  }
+
   render() {
     return (
       <div className="App">
